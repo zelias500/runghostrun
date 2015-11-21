c848df2495a797c38530322d2260aaa83dcbc18b
tmkelly28 <tmkelly28@gmail.com>
2015-11-21 14:06:39 -0500

    Added auth - should totally work after adding oauth keys

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,8 @@
-window.app = angular.module('runghost', ['ionic', 'ngCordova', 'ngMap'])
+window.app = angular.module('runghost', ['ionic', 'ngCordova', 'ngMap', 'fsaPreBuilt'])
 
-app.run(function($ionicPlatform) {
+app.run(function ($ionicPlatform, $rootScope, AuthService, $state, Session) {
 
-    $ionicPlatform.ready(function() {
+    $ionicPlatform.ready(function () {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
         if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
@@ -15,6 +15,43 @@ app.run(function($ionicPlatform) {
           StatusBar.styleDefault();
         }
     });
+
+    // The given state requires an authenticated user.
+    var destinationStateRequiresAuth = function (state) {
+        return state.data && state.data.authenticate;
+    };
+
+    // $stateChangeStart is an event fired
+    // whenever the process of changing a state begins.
+    $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
+
+        if (!destinationStateRequiresAuth(toState)) {
+            // The destination state does not require authentication
+            // Short circuit with return.
+            return;
+        }
+
+        if (AuthService.isAuthenticated()) {
+            // The user is authenticated.
+            // Short circuit with return.
+            return;
+        }
+
+        // Cancel navigating to new state.
+        event.preventDefault();
+
+        AuthService.getLoggedInUser().then(function (user) {
+            // If a user is retrieved, then renavigate to the destination
+            // (the second time, AuthService.isAuthenticated() will work)
+            // otherwise, if no user is logged in, go to "login" state.
+            if (user) {
+                $state.go(toState.name, toParams);
+            } else {
+                $state.go('login');
+            }
+        });
+    });
+
 });
 
 app.config(function($stateProvider, $urlRouterProvider) {
---
+++ www/js/login/login.js
@@ -0,0 +1,26 @@
+app.config(function ($stateProvider) {
+
+    $stateProvider.state('login', {
+        url: '/login',
+        templateUrl: 'js/login/login.html',
+        controller: 'LoginCtrl'
+    });
+
+});
+
+app.controller('LoginCtrl', function ($scope, AuthService, $state) {
+
+    $scope.login = {};
+    $scope.error = null;
+
+    $scope.sendLogin = function (loginInfo) {
+
+        $scope.error = null;
+
+        AuthService.login(loginInfo).then(function (user) {
+            $state.go('tab.home');
+        }).catch(function () {
+            $scope.error = 'Invalid login credentials.';
+        });
+    };
+});
