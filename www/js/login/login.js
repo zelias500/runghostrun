bfd47540083825e614370597e91fb232737c00ad
mbthiery <mbthiery@gmail.com>
2015-11-23 10:52:30 -0500

    removed dead code

--- www/js/login/login.js
+++ www/js/login/login.js
@@ -12,18 +12,4 @@ app.config(function ($stateProvider) {
 });
 
 app.controller('LoginCtrl', function ($scope, AuthService, $state) {
-
-    // $scope.login = {};
-    // $scope.error = null;
-
-    // $scope.sendLogin = function (loginInfo) {
-
-    //     $scope.error = null;
-
-    //     AuthService.login(loginInfo).then(function (user) {
-    //         $state.go('tab.home');
-    //     }).catch(function () {
-    //         $scope.error = 'Invalid login credentials.';
-    //     });
-    // };
 });
