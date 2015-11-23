794fd88fa30997618638419dc291c1a953a30366
Vivi Wen <hsinweiwen@Hsins-MacBook-Pro.local>
2015-11-23 12:48:09 -0500

    add profile pic

--- www/js/tab-profile/tab.profile.js
+++ www/js/tab-profile/tab.profile.js
@@ -9,10 +9,23 @@ app.config(function ($stateProvider) {
                 templateUrl: 'js/tab-profile/tab.profile.html',
                 controller: 'ProfileCtrl'
             }
+        },
+        resolve:{
+            singleUser: function(UserFactory, Session){
+                return UserFactory.fetchById(Session.user._id)
+            }
         }
     });
 });
 
-app.controller('ProfileCtrl', function ($scope) {
-  $scope.something = "Hello we are in Profile!"
+app.controller('ProfileCtrl', function ($scope,singleUser) {
+
+    $scope.me = singleUser
+
+    $scope.myghosts = singleUser.ghosts;
+
+    $scope.myRecentGhost = singleUser.ghosts[singleUser.ghosts.length-1] || "You dont't have any recent ghosts"
+
+     console.log("single user", singleUser);
+     console.log("single user", $scope.myghosts);
 });
