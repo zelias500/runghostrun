7842b4dd2525737b5c8044808237b2f2a7647b4b
tmkelly28 <tmkelly28@gmail.com>
2015-11-24 11:50:42 -0500

    more fixing

---
+++ server/env/development.js
@@ -0,0 +1,19 @@
+module.exports = {	
+  "DATABASE_URI": "mongodb://localhost:27017/ghost",		
+  "SESSION_SECRET": "Optimus Prime is my real dad",		
+  "TWITTER": {		
+    "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",		
+    "consumerSecret": "INSERT_TWITTER_CONSUMER_SECRET_HERE",		
+    "callbackUrl": "INSERT_TWITTER_CALLBACK_HERE"		
+  },		
+  "FACEBOOK": {		
+    "clientID": "435586523303337",		
+    "clientSecret": "89d43b482037abbaab76680cd3983a5e",		
+    "callbackURL": "http://127.0.0.1:3000/auth/facebook/callback"		
+  },		
+  "GOOGLE": {		
+    "clientID": "66361965450916r9dejevr0de7g4t64l0ucjov3rkprc.apps.googleusercontent.com",		
+    "clientSecret": "UGlzuBlj3iCtGpVAHSAB7I4N",		
+    "callbackURL": "http://127.0.0.1:3000/auth/google/callback"		
+  }		
+}; 		
