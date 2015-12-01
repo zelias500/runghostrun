e589d46cfe2bcdb9cd49bfd54897140607cfbbd3
tmkelly28 <tmkelly28@gmail.com>
2015-12-01 09:44:59 -0500

    Stabilized profile more, added followers to user model

--- www/js/tab-profile/tab.profile.js
+++ www/js/tab-profile/tab.profile.js
@@ -12,18 +12,18 @@ app.config(function ($stateProvider) {
             }
         },
         resolve:{
-            singleUser: function (UserFactory, $stateParams) {
+            me: function (UserFactory, $stateParams) {
                 return UserFactory.fetchById($stateParams.id);
-            },
+            }
             // allGhost: function (UserFactory, $stateParams) {
             //     return UserFactory.fetchAllChallenges($stateParams.id);
             // },
-            allFriends:function (UserFactory, $stateParams) {
-                return UserFactory.fetchAllFriends($stateParams.id);
-            },
-            averagePace: function (UserFactory, $stateParams) {
-                return UserFactory.fetchAvgPace($stateParams.id);
-            }
+            // allFriends:function (UserFactory, $stateParams) {
+            //     return UserFactory.fetchAllFriends($stateParams.id);
+            // },
+            // averagePace: function (UserFactory, $stateParams) {
+            //     return UserFactory.fetchAvgPace($stateParams.id);
+            // }
             // averageDis: function (UserFactory, $stateParams) {
             //     return UserFactory.fetchAvgDis($stateParams.id);
             // }
@@ -31,20 +31,26 @@ app.config(function ($stateProvider) {
     })
 });
 
-app.controller('ProfileCtrl', function ($scope, singleUser, UserFactory, allFriends, averagePace, $state) {
-    $scope.successMessage;
-
-    $scope.$on('$ionicView.enter', function(scopes, states) {
-        if (states.stateParams.id != $scope.userId && states.direction == 'swap') {
-            $state.go($state.current, {id: $scope.userId}, {reload: true})
-        }
-    })
-
-    $scope.friends = allFriends
-    $scope.me = singleUser
+app.controller('ProfileCtrl', function ($scope, me, UserFactory, $state) {
+    $scope.me = me;
+    $scope.numFriends = me.friends.length;
+    $scope.numFollowers = me.followers.length;
+    $scope.numRuns = me.ghosts.length;
     $scope.notMe = function () {
-        return !(singleUser._id == $scope.userId);
+        return !(me._id === $scope.userId);
     }
+
+    console.log(me);
+
+    // $scope.successMessage;
+
+    // $scope.$on('$ionicView.enter', function(scopes, states) {
+    //     if (states.stateParams.id != $scope.userId && states.direction == 'swap') {
+    //         $state.go($state.current, {id: $scope.userId}, {reload: true})
+    //     }
+    // })
+
+    // $scope.friends = allFriends
 
     // $scope.myghosts = allGhost
 
@@ -54,17 +60,17 @@ app.controller('ProfileCtrl', function ($scope, singleUser, UserFactory, allFrie
     // $scope.myRecentGhost = allGhost[allGhost.length-1]
 
 
-    $scope.avgPace = Math.floor(averagePace*60) || 0// unit: km/min
+    // $scope.avgPace = Math.floor(averagePace*60) || 0// unit: km/min
     // $scope.avgDis = Math.floor(averageDis) || 0// unit: km
 
-    if($scope.friends && $scope.friends.length){
-        $scope.friends = allFriends.slice(0,3)
-    }
+    // if($scope.friends && $scope.friends.length){
+    //     $scope.friends = allFriends.slice(0,3)
+    // }
 
-    $scope.addToFd = function(){
-        return UserFactory.createFriend($scope.userId, $scope.me._id).then(function(){
-            $scope.successMessage = $scope.me.email + ' added to your friends list!'
-        })
-    }
+    // $scope.addToFd = function () {
+    //     return UserFactory.createFriend($scope.userId, $scope.me._id).then(function() {
+    //         $scope.successMessage = $scope.me.email + ' added to your friends list!'
+    //     });
+    // }
 
 });
