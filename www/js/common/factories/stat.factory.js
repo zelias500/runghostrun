c8feda79f6d4b61a03437103cade43032041ab36
tmkelly28 <tmkelly28@gmail.com>
2015-12-01 21:03:50 -0500

    Clean up pace calc

--- www/js/common/factories/stat.factory.js
+++ www/js/common/factories/stat.factory.js
@@ -4,27 +4,31 @@ app.factory('StatFactory', function () {
 	// takes a run - returns the average pace in min/m
 	factory.minMeter = function (run) {		
 		var distance = run.distance; // distance in meters
-		var time = Math.floor(run.time / 60) // time in minutes
+		var time = (run.time / 60) // time in minutes
 		if (distance === 0) return 0; // prevent dividing by zero
-		return (time / distance);
+		var pace = (time / distance).toFixed(2);
+		return Number(pace);
 	};
 
 	// takes a run - returns the average pace in min/km
 	factory.minKm = function (run) {
 		var distance = run.distance; // distance in meters
-		var time = Math.floor(run.time / 60) // time in minutes
+		var time = (run.time / 60); // time in minutes
 		if ((distance / 1000) === 0) return 0; // prevent dividing by zero
-		return (time / (distance / 1000));
+		var pace = (time / (distance / 1000).toFixed(2));
+		return Number(pace);
 	};
 
 	// converts pace in min/km to min/mile
 	factory.minKm2minMi = function (pace) {
-        return Math.floor(pace * 1.609344);	
+        var pace = (pace * 1.609344).toFixed(2);
+        return Number(pace);
 	};
 
 	// converts distance in km to miles
 	factory.km2mi = function (km) {
-        return Math.floor(km * 0.621371);
+        var pace = (km * 0.621371).toFixed(2);
+        return Number(pace);
 	}
 
 	return factory;
