a4b0500dbc9eb6b6294a729ab1491dc031f1cec4
Zack Elias <zelias500@gmail.com>
2015-12-03 12:30:02 -0500

    run status: validated

--- www/js/results/results.js
+++ www/js/results/results.js
@@ -9,8 +9,11 @@ app.config(function ($stateProvider) {
     });
 });
 
-app.controller('ResultsCtrl', function ($rootScope, $scope, $state, LocationFactory, $stateParams, MapFactory, $timeout, TimeFactory) {
+
+
+app.controller('ResultsCtrl', function ($rootScope, $scope, $state, LocationFactory, $stateParams, MapFactory, $timeout, TimeFactory, ValidationFactory, $ionicPopup) {
     $scope.stopData = LocationFactory.getStopData(); // data from the run
+    console.log('stopdata', $scope.stopData);
     $scope.map = MapFactory.getMap(); // map data from the run
 
     // privacy controls
@@ -33,12 +36,38 @@ app.controller('ResultsCtrl', function ($rootScope, $scope, $state, LocationFact
 
     $scope.save = function() {
         $scope.stopData.privacy = $scope.privacySetting.toLowerCase();
-        LocationFactory.saveRun($rootScope.userId, $scope.stopData)
+
+        if (!$scope.stopData.ghost || ValidationFactory.validateRun($scope.stopData)){
+            $scope.executeSave();
+        }
+        else {
+            $scope.failedValidationPopup();
+        }
+    }
+
+    $scope.failedValidationPopup = function () {
+        var confirmPopup = $ionicPopup.confirm({
+            title: 'Challenge validation failed!',
+            template: "Your run doesn't match the ghost. Save as new ghost?"
+        });
+        confirmPopup.then(function(res){
+            if (res) {
+                delete $scope.stopData.ghost;
+                $scope.executeSave();
+            }
+            else {
+                $scope.discard();
+            }
+        })
+    }
+
+    $scope.executeSave = function() {
+         LocationFactory.saveRun($rootScope.userId, $scope.stopData)
         .then(function (runOrGhost) {
             // if its a ghost
             if (runOrGhost.time) $state.go('tab.ghost', {gid: runOrGhost._id});
             else $state.go('tab.ghost', {gid: runOrGhost.ghost});
-        });
+        });     
     }
 
     $scope.discard = function() {
