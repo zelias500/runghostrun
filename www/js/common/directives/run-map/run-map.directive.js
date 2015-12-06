adcc5941284f3d2bbbf662fa044ecec2cefa46ed
tmkelly28 <tmkelly28@gmail.com>
2015-12-06 10:03:32 -0500

    Removed ghost map, modularized run map directive

--- www/js/common/directives/run-map/run-map.directive.js
+++ www/js/common/directives/run-map/run-map.directive.js
@@ -3,7 +3,9 @@ app.directive('runMap', function (MapFactory, $rootScope, $timeout, LocationFact
 		scope: {
 			runData: '=',	// accepts run data from the location factory during an active run
 			result: '=',	// accepts map data from the map factory post-run
-			challenge: '='	// accepts a possible challenge ghost
+			challenge: '=',	// accepts a possible challenge ghost
+			ghost: '=',		// accepts a ghost or run data for a completed ghost
+			clickable: '='	// accepts a boolean for whether to link to the challenge page
 		},
 		templateUrl: '/js/common/directives/run-map/run-map.html',
 		restrict: 'E',
@@ -20,53 +22,42 @@ app.directive('runMap', function (MapFactory, $rootScope, $timeout, LocationFact
 				});
 			}
 
-			// google maps initialization when challenging
-			function initChallenge () {
-				MapFactory.newMap(scope.challenge, scope.runUniqueId, {
-					showPosition: false,
-					centerOnInitialPosition: true,
-					challengeGhost: scope.challenge
+			function initialize(data, centerOnInitialPosition, showPosition, drawEndPoints, challengeGhost) {
+				MapFactory.newMap(data, scope.runUniqueId, {
+					centerOnInitialPosition: centerOnInitialPosition,
+					showPosition: showPosition,
+					drawEndPoints: drawEndPoints,
+					challengeGhost: challengeGhost
 				}).then(function (map) {
 					scope.map = map;
 				}).then(null, function (err) {
 					console.error(err);
-				})
+				});
 			}
 
-			// google maps initialization when this is a brand new run
-			function initNew () {
-				MapFactory.newMap(scope.runData, scope.runUniqueId, {
-					showPosition: true,
-					centerOnInitialPosition: true
-				}).then(function (map) {
-					scope.map = map;
-				}).then(null, function (err) {
-					console.error(err);
-				})
-			}
-
-			// google maps initialization before starting a new run
-			function initEmpty () {
-				MapFactory.newMap(null, scope.runUniqueId, {
-					showPosition: true,
-					centerOnInitialPosition: true
-				}).then(function (map) {
-					scope.map = map;
-				}).then(null, function (err) {
-					console.error(err);
-				})
-			}
+			if (scope.clickable) scope.address = "#/tab/ghost/" + scope.ghost._id
+			else scope.address = false;
 
 			// process default google map
 			if (scope.result) $timeout(initResult, 0);	// if we have a result map, render using initResult
-			else {
-				if (!scope.challenge) $timeout(initEmpty, 0);	// if about to start a new run, render the map with user's position
-				if (scope.challenge) $timeout(initChallenge, 0)	// if about to challenge, render the map with the challenge route
-			}
+			else if (scope.ghost) $timeout(() => {
+				initialize(scope.ghost, false, false, true, false);
+			}, 0);
+			else if (!scope.challenge) $timeout(() => {
+				initialize(null, true, true, false, false);
+			}, 0);	// if about to start a new run, render the map with user's position
+			else if (scope.challenge) $timeout(() => {
+				initialize(scope.challenge, true, false, false, scope.challenge);
+			}, 0)	// if about to challenge, render the map with the challenge route
+
 
 			// handling of events during an active run
-			var start = $rootScope.$on('start', initNew);
-			var startChallenge = $rootScope.$on('startChallenge', initChallenge);
+			var start = $rootScope.$on('start', () => {
+				initialize(scope.runData, true, true, false, false);
+			});
+			var startChallenge = $rootScope.$on('startChallenge', () => {
+				initialize(scope.challenge, true, false, false, scope.challenge);
+			});
 			var tick = $rootScope.$on('tick', function () {
 				scope.map.tick(scope.runData)
 			});
