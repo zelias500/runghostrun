9a2e4f1def419b45ba80f87a040d786d58eed770
tmkelly28 <tmkelly28@gmail.com>
2015-12-09 15:53:31 -0500

    Trying a new throttle

--- www/js/common/factories/location.factory.js
+++ www/js/common/factories/location.factory.js
@@ -58,15 +58,22 @@ app.factory('LocationFactory', function ($cordovaGeolocation, $rootScope, UserFa
 		// clears location data array and attaches a position watcher
 		startNewRun: function () {
 
-			var _throttle = 0;
+			var _warmUp = 0;
+			var _throttle = false;
 
 			var watchCb = function (pos) {
 
 				// throttle the first several attempts to allow geolocator to warm up
-				while (_throttle < 5) {
-					_throttle++;
+				while (_warmUp < 5) {
+					_warmUp++;
 					return null;
 				}
+
+				// only take every other call from the watcher
+				if (_throttle) {
+					_throttle = !_throttle;
+					return null;
+				} else _throttle = !_throttle;
 
 				pos = {
 					lat: pos.coords.latitude,
